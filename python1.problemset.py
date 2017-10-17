ed6a5d401750409815b227129b8cdf964a5b3115
Bo Wang <bwang@kinton.cshl.edu>
2017-10-17 11:34:57 -0400

    course practice

---
+++ python1.problemset.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+print ("Hello New York")
+
+#####################
+name='BWang'
+
+color='black'
+
+activity='coding'
+
+animal='dog'
+
+print (name,color)
+
+#####################
+import sys
+
+a=sys.argv[1]
+
+b=sys.argv[2]
+
+c=sys.argv[3]
+
+d=sys.argv[4]
+
+print (a,b,c,d)
+
